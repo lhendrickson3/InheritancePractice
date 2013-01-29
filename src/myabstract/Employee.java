5749468ca221b256d410dc3b49cce7c50e9bf13a
lhendrickson3@my.wctc.edu <>
2013-01-29 13:44:22 -0600

    made "Employee" class and abstract class

--- src/myabstract/Employee.java
+++ src/myabstract/Employee.java
@@ -4,10 +4,26 @@
  */
 package myabstract;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author User
  */
-public class Employee {
+public abstract class Employee {
+    
+    private double amountPaid;
+    
+    public void paid() {
+    JOptionPane.showMessageDialog(null, getAmountPaid());
+    }
+    
+    public void setAmountPaid(double Value) {
+        amountPaid = Value;
+    }
+    
+    public double getAmountPaid() {
+        return amountPaid;
+    }
     
 }
--- src/myabstract/SalariedEmployee.java
+++ src/myabstract/SalariedEmployee.java
@@ -4,10 +4,48 @@
  */
 package myabstract;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author User
  */
-public class SalariedEmployee {
+public class SalariedEmployee extends Employee{
+    private double yearsWorked;
     
+    private double salary;
+    
+    private double salaryPay;
+    
+    public void HowManyYearsWorked (){
+        yearsWorked = (Double.parseDouble(JOptionPane.showInputDialog(null, "Enter how many years have you worked?")));
+    }
+    
+    public void YearlySalary (){
+        salary = (Double.parseDouble(JOptionPane.showInputDialog(null, "What is your salary?")));
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    
+    public double getYearsWorked() {
+        return yearsWorked;
+    }
+
+    public void setYearsWorked(double yearsWorked) {
+        this.yearsWorked = yearsWorked;
+    }
+    
+     public void setSalaryPay(){
+    salaryPay = (getYearsWorked() * getSalary());
+    }
+    
+    public double getSalaryPay(){
+    return salaryPay;
+    }    
 }
